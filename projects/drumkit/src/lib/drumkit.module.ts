4f2147fc12bf1b0564210588fdd8d0db79dabae5
web-dave <dave@webdave.de>
2018-11-14 11:28:33 +0100

    use drumkit

--- projects/drumkit/src/lib/drumkit.module.ts
+++ projects/drumkit/src/lib/drumkit.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { DrumkitComponent } from './drumkit.component';
-import { ClapComponent } from './clap/clap.component';
-import { TomComponent } from './tom/tom.component';
-import { KickComponent } from './kick/kick.component';
+import { NgModule } from "@angular/core";
+import { DrumkitComponent } from "./drumkit.component";
+import { ClapComponent } from "./clap/clap.component";
+import { TomComponent } from "./tom/tom.component";
+import { KickComponent } from "./kick/kick.component";
 
 @NgModule({
   declarations: [DrumkitComponent, ClapComponent, TomComponent, KickComponent],
-  imports: [
-  ],
-  exports: [DrumkitComponent]
+  imports: [],
+  exports: [DrumkitComponent, ClapComponent, TomComponent, KickComponent]
 })
-export class DrumkitModule { }
+export class DrumkitModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DrumkitModule } from "projects/drumkit/src/public_api";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DrumkitModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
