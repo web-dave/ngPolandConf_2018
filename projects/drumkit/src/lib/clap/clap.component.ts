1e4a50be380d5b6eb3a06a59bc1ea80ca5301c7e
web-dave <dave@webdave.de>
2018-11-14 11:20:07 +0100

    clap component

--- projects/drumkit/src/lib/clap/clap.component.ts
+++ projects/drumkit/src/lib/clap/clap.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'lib-clap',
-  templateUrl: './clap.component.html',
-  styleUrls: ['./clap.component.css']
+  selector: "clap",
+  templateUrl: "./clap.component.html",
+  styleUrls: ["./clap.component.scss"]
 })
 export class ClapComponent implements OnInit {
+  @Input() frequency: number = 1000;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  clap() {
+    let context = new AudioContext();
+    let noiseBuffer = context.createBuffer(1, 44100, 44100);
+    let noiseBufferOutput = noiseBuffer.getChannelData(0);
+    for (var i = 0; i < 44100; i++) {
+      noiseBufferOutput[i] = Math.random() * 2 - 1;
+    }
+    let noise = context.createBufferSource();
+    noise.buffer = noiseBuffer;
+    let noiseFilter = context.createBiquadFilter();
+    noiseFilter.type = "highpass";
+    noiseFilter.frequency.value = this.frequency;
+    noise.connect(noiseFilter);
+    let noiseEnvelope = context.createGain();
+    noiseFilter.connect(noiseEnvelope);
+    noiseEnvelope.connect(context.destination);
+    noiseEnvelope.gain.setValueAtTime(0.7, 0);
+    noiseEnvelope.gain.exponentialRampToValueAtTime(0.5, 0.05);
+    noiseEnvelope.gain.exponentialRampToValueAtTime(0.01, 0.1);
+    noise.start();
+    noise.stop(0.1);
   }
-
 }
